5899ebc85c92018d28a16a7117b032664ff710df
Camilo <Camilo@157.253.165.155>
2014-04-01 15:38:03 -0500

    Test2

--- CentralAbastos/src/co/edu/uniandes/centralAbastos/dao/DAOPedidosEfectivos.java
+++ CentralAbastos/src/co/edu/uniandes/centralAbastos/dao/DAOPedidosEfectivos.java
@@ -23,8 +23,6 @@ public class DAOPedidosEfectivos extends ConsultaDAO
 	
 	private static final String ID= "ID_PEDIDO";
 	
-	private static final String test = "test";
-	
 	private static final String CORREO = "CORREO_PROVEEDOR"; 
 	
 	private static final String FECHA = "FECHA_LLEGADA"; 
