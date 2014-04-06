7fb5276c6aff9fa750e040bcf22475ab47cd601a
paotoya757 <pa.otoya757@uniandes.edu.co>
2014-04-05 19:18:40 -0500

    d

---
+++ CentralAbastos/src/co/edu/uniandes/centralAbastos/fachada/ModLocal.java
@@ -0,0 +1,16 @@
+package co.edu.uniandes.centralAbastos.fachada;
+
+import co.edu.uniandes.centralAbastos.dao.DAOAlmacen;
+
+public class ModLocal 
+{
+	private DAOAlmacen dao;
+
+	public ModLocal(DAOAlmacen dao) 
+	{
+		this.dao = dao;
+	}
+	
+	
+	
+}
